de04e9e947f9293ec97b9341a248ac1bbf74d2ab
Favour F Atilade <favyatil@gmail.com>
2025-11-22 16:45:43 +0100

    test case.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use json_parser::lexen;
+use json_parser::lexen::Lexer;
+use json_parser::read_file;
 
 fn main() {
-    println!("Hello, world!");
-    let s = "number-".to_string();
-    let b: bool = s.ends_with("-");
-    println!("s ends with {b}");
+    let read = match read_file("json_obj.txt") {
+        Ok(suc) => suc,
+        Err(err) =>
+        // eprintln!("file read failure, {}", err);
+        {
+            "error".to_string()
+        }
+    };
+
+    println!("{}", read);
+    let lexer: Lexer = Lexer::new(read);
 }
